14e2ed723722d494e3652c8c74d55d9742be7e0a
thiago7855 <tpccastro@gmail.com>
2025-04-07 21:50:21 +0000

    printf "xadrez\n" > xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
 
-
-
 int main() {
     // mover a torre 5 casas para direita
     printf("Movimentação da Torre:\n");
-    for (int i = 0; i < 5; i++) 
-    {
+    for (int i = 0; i < 5; i++) {
         printf("direita\n");
     }
+
     // mover o bispo 5 casas diagonal para direita
     printf("Movimentação do Bispo:\n");
     int casa_bispo = 5;
-    for (int i = 0; i < casa_bispo; i++) 
-    {
+    for (int i = 0; i < casa_bispo; i++) {
         printf("diagonal direita\n");
     }
+
     // mover a rainha 8 casas para esquerda
     printf("Movimentação da Rainha:\n");
     int casa_rainha = 0;
-    do
-    {
-    printf("esquerda\n");
-    casa_rainha++;                  
+    do {
+        printf("esquerda\n");
+        casa_rainha++;
     } while (casa_rainha < 8);
 
-    printf("Fim do desafio xadrez");
-    
-    return 0;
+    // Finalização do programa
+    printf("Fim do desafio xadrez\n");
 
+    return 0;
 }
-    
 
   
