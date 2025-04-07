785d72f9bb67dded3ad05a217f4fb1bf8a6def49
thiago7855 <tpccastro@gmail.com>
2025-04-07 23:31:50 +0000

    desafio xadrez atualizado

--- xadrez.c
+++ xadrez.c
@@ -2,20 +2,20 @@
 
 int main() {
     // mover a torre 5 casas para direita
-    printf("Movimentação da Torre:\n");
+    printf("\nMovimentação da Torre:\n");
     for (int i = 0; i < 5; i++) {
         printf("direita\n");
     }
 
     // mover o bispo 5 casas diagonal para direita
-    printf("Movimentação do Bispo:\n");
+    printf("\nMovimentação do Bispo:\n");
     int casa_bispo = 5;
     for (int i = 0; i < casa_bispo; i++) {
         printf("diagonal direita\n");
     }
 
     // mover a rainha 8 casas para esquerda
-    printf("Movimentação da Rainha:\n");
+    printf("\nMovimentação da Rainha:\n");
     int casa_rainha = 0;
     do {
         printf("esquerda\n");
